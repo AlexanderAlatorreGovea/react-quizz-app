028de0eb4a21b25565d1a7db96e1b782520511c9
Alexander Govea <alexander_alatorre1993@hotmail.com>
2021-10-11 21:19:03 -0600

    first commit

---
+++ src/API.ts
@@ -0,0 +1,36 @@
+import { shuffleArray } from "./utils";
+
+export type Question = {
+  category: string;
+  correct_answer: string;
+  difficulty: string;
+  incorrect_answers: string[];
+  question: string;
+  type: string;
+};
+
+export enum Difficulty {
+  EASY = "easy",
+  MEDIUM = "medium",
+  HARD = "hard",
+}
+
+//Adds property answers
+export type QuestionsState = Question & { answers: string[] };
+
+export const fetchQuizQuestions = async (
+  amount: number,
+  difficulty: Difficulty
+): Promise<QuestionsState[]> => {
+  const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
+  const data = await (await fetch(endpoint)).json();
+
+  return data.results.map((question: Question) => ({
+    ...question,
+    answers: shuffleArray([
+      //spread all the incorrect answers and add the correct answer
+      ...question.incorrect_answers,
+      question.correct_answer,
+    ]),
+  }));
+};
